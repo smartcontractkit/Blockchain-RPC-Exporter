ab6dceb0edf6f9dfda28040e156d2c216a2c95b2
Namik Mesic <namikmesic@gmail.com>
2022-09-06 11:33:17 +0200

    Complete rewrite

---
+++ src/collectors/conflux.py
@@ -0,0 +1,51 @@
+from settings import cfg, logger
+from conflux_web3 import Web3
+import asyncio
+from helpers import strip_url
+from collectors.ws import websocket_collector
+
+
+class conflux_collector():
+
+    def __init__(self, websocket_url, https_url, provider):
+        self.client = Web3(Web3.WebsocketProvider(websocket_url, websocket_timeout=cfg.response_timeout))
+        self.labels = [
+            'websocket_url', 'https_url', 'provider', 'blockchain', 'network_name', 'network_type', 'evmChainID'
+        ]
+        self.labels_values = [
+            websocket_url, https_url, provider, cfg.blockchain, cfg.network_name, cfg.network_type,
+            str(cfg.chain_id)
+        ]
+        self.ws_collector = websocket_collector(websocket_url,
+                                                sub_payload={
+                                                    "method": "cfx_subscribe",
+                                                    "jsonrpc": "2.0",
+                                                    "id": 1,
+                                                    "params": ["newHeads"]
+                                                })
+        self.ws_collector.setDaemon(True)
+        self.ws_collector.start()
+        self.first_time = False
+
+    def probe(self, metrics):
+        try:
+            if self.client.isConnected():
+                metrics['ws_rpc_health'].add_metric(self.labels_values, True)
+                metrics['ws_rpc_head_count'].add_metric(self.labels_values, self.ws_collector.message_counter)
+                metrics['ws_rpc_disconnects'].add_metric(self.labels_values, self.ws_collector.disconnects_counter)
+                metrics['ws_rpc_latency'].add_metric(self.labels_values, self.ws_collector.get_latency())
+                metrics['ws_rpc_block_height'].add_metric(self.labels_values, self.client.cfx.epoch_number)
+
+                metrics['ws_rpc_difficulty'].add_metric(
+                    self.labels_values,
+                    self.client.cfx.get_block_by_hash(self.client.cfx.get_best_block_hash())['difficulty'])
+
+                metrics['ws_rpc_gas_price'].add_metric(self.labels_values, self.client.cfx.gas_price)
+            else:
+                logger.info("Client is not connected to {}".format(strip_url(self.websocket_url)))
+                metrics['ws_rpc_health'].add_metric(self.labels_values, False)
+        except asyncio.exceptions.TimeoutError:
+            logger.info("Client timed out for {}".format(strip_url(self.websocket_url)))
+            metrics['ws_rpc_health'].add_metric(self.labels_values, False)
+        except Exception as e:
+            print(e, type(e))
--- src/exporter.py
+++ src/exporter.py
@@ -1,150 +1,99 @@
-import os
-import logging
-
+from concurrent.futures import ThreadPoolExecutor
 from prometheus_client import REGISTRY, make_wsgi_app
 from prometheus_client.metrics_core import GaugeMetricFamily, CounterMetricFamily
 from wsgiref.simple_server import make_server
-
-import settings
-from probe import rpc_probe
-
-from concurrent.futures import ThreadPoolExecutor
-
-LOGLEVEL = settings.LOGLEVEL
-logging.basicConfig(level=LOGLEVEL, format='%(asctime)s - %(message)s')
+from helpers import strip_url
+from collectors.evm import evm_collector
+from collectors.solana import solana_collector
+from collectors.cardano import cardano_collector
+from collectors.conflux import conflux_collector
+from settings import logger, cfg
 
 
-class metrics_registry(object):
+class prom_registry(object):
 
-    def __init__(self, collector=None):
-        if not collector:
-            collector = rpc_probe(uris=settings.RPCS,
-                                  timeout=settings.TIMEOUT,
-                                  chain_id=settings.CHAIN_ID)
-            collector.setDaemon(True)
-            collector.start()
+    def __init__(self):
+        self.collectors = []
+        self.labels = []
 
-        self._collector = collector
+        if cfg.isEvm():
+            self._instantiate_evm()
+        if cfg.isSolana():
+            self._instantiate_solana()
+        if cfg.isCardano():
+            self._instantiate_cardano()
+        if cfg.isConflux():
+            self._instantiate_conflux()
 
-    def _highest_in_results(self, results: list, target_key: str):
-        highest = 0
-        for key in results:
-            obs_value = key[target_key]
-            if obs_value > highest:
-                highest = obs_value
-        return highest
+    def _instantiate_evm(self):
+        for item in cfg.endpoints:
+            logger.info("Initializing evm node {}".format(strip_url(item['ws_url'])))
+            self.collectors.append(evm_collector(item['ws_url'], item['https_url'], item['provider']))
+        self.labels = self.collectors[0].labels
+
+    def _instantiate_conflux(self):
+        for item in cfg.endpoints:
+            logger.info("Initializing conflux node {}".format(strip_url(item['ws_url'])))
+            self.collectors.append(conflux_collector(item['ws_url'], item['https_url'], item['provider']))
+        self.labels = self.collectors[0].labels
+
+    def _instantiate_solana(self):
+        for item in cfg.endpoints:
+            logger.info("Initializing solana node {}".format(strip_url(item['ws_url'])))
+            self.collectors.append(solana_collector(item['ws_url'], item['https_url'], item['provider']))
+        self.labels = self.collectors[0].labels
+
+    def _instantiate_cardano(self):
+        for item in cfg.endpoints:
+            logger.info("Initializing cardano node {}".format(strip_url(item['ws_url'])))
+            self.collectors.append(cardano_collector(item['ws_url'], item['provider']))
+            self.labels = self.collectors[0].labels
 
     def collect(self):
-        metric_labels = [
-            'url', 'blockchain', 'network_name', 'network_type', 'url',
-            'provider'
-        ]
+        metrics = {
+            "ws_rpc_health":
+            GaugeMetricFamily('ws_rpc_health',
+                              'Returns 1 if rpc websocket server established a connection with the probe client.',
+                              labels=self.labels),
+            "ws_rpc_latency":
+            GaugeMetricFamily('ws_rpc_latency',
+                              'Latency in milliseconds of the websocket keepalive ping.',
+                              labels=self.labels),
+            "ws_rpc_disconnects":
+            GaugeMetricFamily('ws_rpc_disconnects', 'How many times rpc has disconnected.', labels=self.labels),
+            "ws_rpc_block_height":
+            GaugeMetricFamily('ws_rpc_block_height', 'Latest observed block_height.', labels=self.labels),
+            "ws_rpc_head_count":
+            CounterMetricFamily('ws_rpc_head_count', 'Heads received total.', labels=self.labels),
+            "ws_rpc_difficulty":
+            GaugeMetricFamily('ws_rpc_difficulty', 'Difficulty of the latest observed block.', labels=self.labels),
+            "ws_rpc_total_difficulty":
+            GaugeMetricFamily('ws_rpc_total_difficulty',
+                              'Total canonical chain difficulty observed from the first to the latest block.',
+                              labels=self.labels),
+            "ws_rpc_gas_price":
+            GaugeMetricFamily('ws_rpc_gas_price', 'The current gas price in Wei.', labels=self.labels),
+            "ws_rpc_max_priority_fee":
+            GaugeMetricFamily('ws_rpc_max_priority_fee',
+                              'Suggested max priority fee for dynamic fee transactions in Wei.',
+                              labels=self.labels),
+            "ws_rpc_net_peer_count":
+            GaugeMetricFamily('ws_rpc_net_peer_count',
+                              'Number of peers currently connected to the client.',
+                              labels=self.labels)
+        }
 
-        up_metric = GaugeMetricFamily('ws_rpc_health',
-                                      '1 for up 0 for down',
-                                      labels=metric_labels)
-        block_height_metric = GaugeMetricFamily('ws_rpc_block_height',
-                                                'Latest observed block_height',
-                                                labels=metric_labels)
+        def write_metrics(prom_collector, metrics):
+            prom_collector.probe(metrics)
 
-        total_difficulty_metric = GaugeMetricFamily(
-            'ws_rpc_total_difficulty',
-            'Total difficulty observed from the latest block.',
-            labels=metric_labels)
+        with ThreadPoolExecutor(max_workers=len(self.collectors)) as executor:
+            for collector in self.collectors:
+                executor.submit(write_metrics, collector, metrics)
 
-        latency_metric = GaugeMetricFamily(
-            'ws_rpc_latency',
-            'Latency of a perticular RPC endpoint',
-            labels=metric_labels)
-
-        behind_highest_metric_difficulty = GaugeMetricFamily(
-            'ws_rpc_difficulty_behind_highest',
-            'How much latest block a perticular node deviates from latest observed totalDifficulty in the pool.',
-            labels=metric_labels)
-
-        behind_highest_metric_blocks = GaugeMetricFamily(
-            'ws_rpc_block_height_behind_highest',
-            'How much blocks a perticular node deviates from highest observed',
-            labels=metric_labels)
-
-        head_count_metric = CounterMetricFamily(
-            'ws_rpc_head_count',
-            'Count of heads received for a perticular rpc endpoint',
-            labels=metric_labels)
-
-        disconnect_counter_metric = CounterMetricFamily(
-            'ws_rpc_disconnects',
-            'How many times rpc has disconnected from the server side',
-            labels=metric_labels)
-
-        within_sla_metric = GaugeMetricFamily(
-            'ws_rpc_sla_compliant',
-            'Based on block height divergence,latency and health, reports 1 for compliant and 0 for non compliant',
-            labels=metric_labels)
-
-        def _record_metrics(record):
-            obs_url = record['url']
-            obs_provider = record['provider']
-            obs_up = record['up']
-            obs_block_height = record['block_height']
-            obs_total_difficulty = record['total_difficulty']
-            obs_latency = record['latency']
-            obs_head_count = head_counts[obs_url]
-            obs_disconnects_count = disconnect_counts[obs_url]
-            total_difficulty_deviation = highest_difficulty - record[
-                'total_difficulty']
-            block_deviation = highest_block - record['block_height']
-
-            labels = [
-                obs_url, settings.BLOCKCHAIN, settings.NETWORK_NAME,
-                settings.NETWORK_TYPE, obs_url, obs_provider
-            ]
-
-            if obs_up:
-                if obs_latency < settings.MAX_RESPONSE_LATENCY_MILISECONDS and block_deviation < settings.MAX_POOL_DEVIATION_BLOCK_COUNT:
-                    within_sla_metric.add_metric(labels, 1)
-                else:
-                    within_sla_metric.add_metric(labels, 0)
-                up_metric.add_metric(labels, obs_up)
-                block_height_metric.add_metric(labels, obs_block_height)
-                total_difficulty_metric.add_metric(labels,
-                                                   obs_total_difficulty)
-                behind_highest_metric_difficulty.add_metric(
-                    labels, total_difficulty_deviation)
-                behind_highest_metric_blocks.add_metric(
-                    labels, block_deviation)
-                latency_metric.add_metric(labels, obs_latency)
-                head_count_metric.add_metric(labels, obs_head_count)
-                disconnect_counter_metric.add_metric(labels,
-                                                     obs_disconnects_count)
-            else:
-                up_metric.add_metric(labels, obs_up)
-
-        results = self._collector.get_results()
-        highest_block = self._highest_in_results(results, 'block_height')
-        highest_difficulty = self._highest_in_results(results,
-                                                      'total_difficulty')
-
-        head_counts = self._collector.head_counter
-        disconnect_counts = self._collector.disconnects_counter
-
-        logging.debug("Results: {}".format(results))
-        logging.debug("Highest block: {}".format(highest_block))
-        logging.debug("Log counts: {}".format(head_counts))
-        logging.debug("Disconnect counts: {}".format(disconnect_counts))
-
-        with ThreadPoolExecutor(max_workers=3) as executor:
-            executor.map(_record_metrics, results)
-
-        yield up_metric
-        yield block_height_metric
-        yield total_difficulty_metric
-        yield behind_highest_metric_difficulty
-        yield latency_metric
-        yield behind_highest_metric_blocks
-        yield head_count_metric
-        yield disconnect_counter_metric
-        yield within_sla_metric
+        for _, metric in metrics.items():
+            # Only yield metric if samples were provided by the probe
+            if len(metric.samples) > 0:
+                yield metric
 
 
 def dummy_report(environ, start_fn):
@@ -155,7 +104,6 @@ def dummy_report(environ, start_fn):
 def my_app(environ, start_fn):
     if environ['PATH_INFO'] == '/metrics':
         return metrics_app(environ, start_fn)
-    # TODO: Write an actual liveness and readiness probes.
     if environ['PATH_INFO'] == '/readiness':
         return dummy_report(environ, start_fn)
     if environ['PATH_INFO'] == '/liveness':
@@ -163,7 +111,8 @@ def my_app(environ, start_fn):
 
 
 if __name__ == '__main__':
-    REGISTRY.register(metrics_registry())
+    REGISTRY.register(prom_registry())
+    logger.info("Started app")
     metrics_app = make_wsgi_app()
     httpd = make_server('', 8000, my_app)
     httpd.serve_forever()
