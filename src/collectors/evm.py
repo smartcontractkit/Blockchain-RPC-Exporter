20c564ddfd81c47a69ab2eac3fb2205147098046
Namik Mesic <namikmesic@gmail.com>
2022-09-15 11:54:28 +0200

    Catch websocket exceptions for EVM collector since client does not.

--- src/collectors/evm.py
+++ src/collectors/evm.py
@@ -3,7 +3,7 @@ from web3 import Web3
 import asyncio
 from helpers import strip_url
 from collectors.ws import websocket_collector
-
+from websockets.exceptions import WebSocketException
 
 class evm_collector():
 
@@ -27,7 +27,7 @@ class evm_collector():
         self.ws_collector.setDaemon(True)
         self.ws_collector.start()
 
-    def probe(self, metrics):
+    def probe(self, metrics):        
         try:
             if self.client.isConnected():
                 metrics['ws_rpc_health'].add_metric(self.labels_values, True)
@@ -45,8 +45,12 @@ class evm_collector():
             else:
                 logger.info("Client is not connected to {}".format(strip_url(self.websocket_url)))
                 metrics['ws_rpc_health'].add_metric(self.labels_values, False)
-        except asyncio.exceptions.TimeoutError:
-            logger.info("Client timed out for {}".format(strip_url(self.websocket_url)))
+        except asyncio.exceptions.TimeoutError as exc:
+            logger.info("Client timed out for {}: {}".format(strip_url(self.websocket_url), exc))
+            metrics['ws_rpc_health'].add_metric(self.labels_values, False)
+        except WebSocketException as exc:
+            logger.info("Websocket client exception {}: {}".format(strip_url(self.websocket_url), exc))
             metrics['ws_rpc_health'].add_metric(self.labels_values, False)
         except Exception as exc:
             logger.error("Failed probing {} with error: {}".format(strip_url(self.url), exc))
+            metrics['ws_rpc_health'].add_metric(self.labels_values, False)
