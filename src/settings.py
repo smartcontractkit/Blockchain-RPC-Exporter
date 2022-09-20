8ca789ebe2aec21dd31032daabd9adb242e6022f
namikmesic <namikmesic@gmail.com>
2022-09-20 15:59:00 +0200

    Add Filecoin (#14)

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ class configuration():
         self.configuration = self._load_configuration_file(config_file_path)
         self.blockchain = self.configuration['blockchain']
         # Load chain_id only if evm compatible collector
-        if self.configuration['collector'] not in ['cardano', 'solana', 'bitcoin', 'doge']:
+        if self.configuration['collector'] not in ['cardano', 'solana', 'bitcoin', 'doge' , 'filecoin']:
             try:
                 self.chain_id = self.configuration['chain_id']
             except KeyError:
@@ -82,6 +82,9 @@ class configuration():
     def isDoge(self) -> bool:
         return self.configuration['collector'] == "doge"
 
+    def isFilecoin(self) -> bool:
+        return self.configuration['collector'] == "filecoin"
+
     def _load_configuration_file(self, path):
         logger.info('Loading {}'.format(path))
         configuration_file_schema = Schema({
@@ -94,7 +97,7 @@ class configuration():
             'network_type':
             And(str, lambda s: s in ('Testnet', 'Mainnet')),
             'collector':
-            And(str, lambda s: s in ('evm', 'cardano', 'conflux', 'solana', 'bitcoin', 'doge')),
+            And(str, lambda s: s in ('evm', 'cardano', 'conflux', 'solana', 'bitcoin', 'doge', 'filecoin')),
             Optional('connection_parameters'): {
                 Optional('open_timeout'): And(int),
                 Optional('close_timeout'): And(int),
