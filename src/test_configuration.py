7aa81ba8342ed5ee3dbb22018a2d721f8444dc3c
Justin Franco <justin.franco@smartcontract.com>
2023-01-13 11:45:21 -0700

    sre-853 Adds additional unit tests to config class

--- src/test_configuration.py
+++ src/test_configuration.py
@@ -1,23 +1,35 @@
-#pylint: disable=protected-access,invalid-name
+# pylint: disable=protected-access,invalid-name
 """Module for testing Config"""
 
 import os
 from unittest import TestCase, mock
+from structlog.testing import capture_logs
+
 from configuration import Config
+
+CONFIG_FILES = {"valid": "tests/fixtures/configuration.yaml",
+                "invalid": "tests/fixtures/configuration_invalid.yaml",
+                "client_params": "tests/fixtures/configuration_conn_params.yaml"}
+VALIDATION_FILE = {"valid": "tests/fixtures/validation.yaml"}
 
 
 class TestConfiguration(TestCase):
     """Tests all configuration.Config attributes used externally."""
 
-    @mock.patch.dict(
-        os.environ, {
-            "CONFIG_FILE_PATH": "tests/fixtures/configuration.yaml",
-            "VALIDATION_FILE_PATH": "tests/fixtures/validation.yaml"
-        })
     def setUp(self):
-        """Set up dummy config for us."""
+        """Set up dummy configs for us."""
         self.maxDiff = None
-        self.config = Config()
+        self.config = setup_config_object(
+            CONFIG_FILES["valid"], VALIDATION_FILE["valid"])
+        self.client_params_config = setup_config_object(
+            CONFIG_FILES["client_params"], VALIDATION_FILE["valid"])
+        self.invalid_config = setup_config_object(
+            CONFIG_FILES["invalid"], VALIDATION_FILE["valid"])
+
+    def test_invalid_get_property(self):
+        """Tests getting invalid properties returns None type"""
+        config_property = self.config.get_property('invalid')
+        self.assertEqual(config_property, None)
 
     def test_configuration_attribute(self):
         """Make sure that the configuration is what we expect after loading."""
@@ -56,23 +68,17 @@ class TestConfiguration(TestCase):
         }
         self.assertEqual(self.config.client_parameters, expected)
 
-    @mock.patch.dict(
-        os.environ, {
-            "CONFIG_FILE_PATH":
-            "tests/fixtures/configuration_conn_params.yaml",
-            "VALIDATION_FILE_PATH": "tests/fixtures/validation.yaml"
-        })
     def test_client_parameters_attribute_present(self):
         """Make sure we use explicitly provided client parameters in the config if
         they are set."""
-        cfg = Config()
         expected = {
             'open_timeout': 1,
             'close_timeout': 2,
             'ping_interval': 3,
             'ping_timeout': 4
         }
-        self.assertEqual(cfg.client_parameters, expected)
+        self.assertDictEqual(
+            self.client_params_config.client_parameters, expected)
 
     def test_endpoints_attribute(self):
         """Make sure we parsed endpoints correctly as expected by external
@@ -88,3 +94,28 @@ class TestConfiguration(TestCase):
             'url': 'wss://test3.com'
         }]
         self.assertEqual(expected, self.config.endpoints)
+
+    def test_load_and_validate_schema_exception_exit(self):
+        """Tests that the program exits when loading a configuration file with a schema exception"""
+        with self.assertRaises(SystemExit) as cm:
+            self.invalid_config._configuration
+        self.assertEqual(1, cm.exception.code)
+
+    def test_load_and_validate_schema_execption_error_log(self):
+        """Tests that an error is logged when loading a config file with a schema exception"""
+        try:
+            with capture_logs() as captured:
+                self.invalid_config._configuration
+        except SystemExit:
+            pass
+        self.assertEqual("error", captured[0]['log_level'])
+
+
+def setup_config_object(config_file, validation_file) -> Config:
+    """Creates a Config object using the provided config and validation files"""
+    with mock.patch.dict(
+            os.environ, {
+                "CONFIG_FILE_PATH": config_file,
+                "VALIDATION_FILE_PATH": validation_file
+            }):
+        return Config()
