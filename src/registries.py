643f3c02e99286bab3335bee44bad14d8439d64d
Justin Franco <justin.franco@smartcontract.com>
2023-01-16 22:42:41 -0700

    sre-853 WIP Refactors registries class Updates collector/endpoint to return lists Also updates pre-commit config to use python 3.11. Since match is used in code python 3.10+ is required.

--- src/registries.py
+++ src/registries.py
@@ -3,10 +3,10 @@ from configuration import Config
 import collectors
 
 
-class Endpoint():  #pylint: disable=too-few-public-methods
+class Endpoint():  # pylint: disable=too-few-public-methods
     """RPC Endpoint class, to store metadata."""
 
-    def __init__(  #pylint: disable=too-many-arguments
+    def __init__(  # pylint: disable=too-many-arguments
             self, url, provider, blockchain, network_name, network_type,
             chain_id, **client_parameters):
         self.url = url
@@ -32,65 +32,54 @@ class EndpointRegistry(Config):
         return self.get_property('collector')
 
     @property
-    def get_endpoint_registry(self) -> dict:
+    def get_endpoint_registry(self) -> list:
         """Iterates trough all of the endpoints and instantiates
         them as Endpoints class in a dict. Returns the populated dict."""
-        return_dict = []
+        endpoints_list = []
         for item in self.endpoints:
-            return_dict.append(
+            endpoints_list.append(
                 Endpoint(item['url'], item['provider'],
                          self.get_property('blockchain'),
                          self.get_property('network_name'),
                          self.get_property('network_type'),
                          self.get_property('chain_id'),
                          **self.client_parameters))
-        return return_dict
+        return endpoints_list
 
 
 class CollectorRegistry(EndpointRegistry):
     """A registry of all collectors."""
 
     @property
-    def get_collector_registry(self) -> dict:
+    def get_collector_registry(self) -> list:
         """Iterates trough all of the instantiated endpoints and loads
         proper collector type based on the collector and chain name."""
-        return_dict = []
+        collectors_list = []
 
         for item in self.get_endpoint_registry:
+            collector = None
             match self.collector, self.blockchain:
                 case "evm", "conflux":
-                    return_dict.append(
-                                collectors.ConfluxCollector(item.url,
-                                item.labels, item.chain_id,
-                                **self.client_parameters))
+                    collector = collectors.ConfluxCollector
                 case "cardano", "cardano":
-                    return_dict.append(
-                                collectors.CardanoCollector(item.url,
-                                item.labels, item.chain_id,
-                                **self.client_parameters))
+                    collector = collectors.CardanoCollector
                 case "bitcoin", "bitcoin":
-                    return_dict.append(
-                                collectors.BitcoinCollector(item.url,
-                                item.labels, item.chain_id,
-                                **self.client_parameters))
+                    collector = collectors.BitcoinCollector
                 case "filecoin", "filecoin":
-                    return_dict.append(
-                                collectors.FilecoinCollector(item.url,
-                                item.labels, item.chain_id,
-                                **self.client_parameters))
+                    collector = collectors.FilecoinCollector
                 case "solana", "solana":
-                    return_dict.append(
-                                collectors.SolanaCollector(item.url,
-                                item.labels, item.chain_id,
-                                **self.client_parameters))
+                    collector = collectors.SolanaCollector
                 case "starkware", "starkware":
-                    return_dict.append(
-                                collectors.StarkwareCollector(item.url,
-                                item.labels, item.chain_id,
-                                **self.client_parameters))
-                case "evm", other: ##pylint: disable=unused-variable
-                    return_dict.append(
-                                collectors.EvmCollector(item.url,
-                                item.labels, item.chain_id,
-                                **self.client_parameters))
-        return return_dict
+                    collector = collectors.StarkwareCollector
+                case "evm", other:  # pylint: disable=unused-variable
+                    collector = collectors.EvmCollector
+            if collector is None:
+                # TODO log this error properly
+                self._logger.error(
+                    f"collector: {self.collector} on bloackchain: {self.blockchain} not found"
+                )
+            else:
+                collectors_list.append(collector(item.url,
+                                                 item.labels, item.chain_id,
+                                                 **self.client_parameters))
+        return collectors_list
