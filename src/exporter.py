fd6aead8c59dd342a71091e07eba4bc1aac1062e
rodgerjohnson <rodger@smartcontract.com>
2024-03-12 23:20:27 +0900

    Separate Liveness check proc to not block requests

--- src/exporter.py
+++ src/exporter.py
@@ -2,7 +2,7 @@
 from wsgiref.simple_server import make_server
 from prometheus_client import REGISTRY, make_wsgi_app
 from metrics import PrometheusCustomCollector
-
+import threading
 
 def return200(_, start_fn):
     """Wsgi http response function."""
@@ -21,16 +21,23 @@ def exporter(environ, start_fn):  # pylint: disable=inconsistent-return-statemen
     match environ['PATH_INFO']:
         case '/metrics':
             return metrics_app(environ, start_fn)
+        case _:
+            return return404(environ, start_fn)
+
+def liveness(environ, start_fn):
+    match environ['PATH_INFO']:
         case '/readiness':
             return return200(environ, start_fn)
         case '/liveness':
             return return200(environ, start_fn)
-        case _:
-            return return404(environ, start_fn)
 
+def start_liveness():
+    httpd = make_server('', 8001, liveness)
+    httpd.serve_forever()
 
 if __name__ == '__main__':
     REGISTRY.register(PrometheusCustomCollector())
     metrics_app = make_wsgi_app()
+    liveness_thread = threading.Thread(target=start_liveness).start()
     httpd = make_server('', 8000, exporter)
     httpd.serve_forever()
